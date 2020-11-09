11db97dff6f15d8aea513bb17d497e81d18652ac
AndikaAryWiguna <71689485+AndikaAryWiguna@users.noreply.github.com>
2020-11-09 22:25:43 +0800

    Add files via upload

---
+++ home.php
@@ -0,0 +1,6 @@
+<center><br><br><br><br><br><br><br><br>
+<b><img src="assets/images/logo.PNG" width="400px height="400px" /> <br>
+<font Size="6" face="Helvetica">Sistem Informasi Mahasiswa</font> <br>
+<font Size="6">Bidik Misi dan Berprestasi</font>
+</b>
+</center>
---
+++ koneksi.php
@@ -0,0 +1,11 @@
+<?php
+$conn = mysqli_connect("localhost","root","","akademik");
+
+/*if($conn){
+    echo"Koneksi Sukses";
+}else{
+    echo"Koneksi Gagal";
+}*/
+
+
+?>
